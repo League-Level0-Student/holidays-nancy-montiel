9cff657d7ad2155b2674ac3dfa4c0ae82eb87dde
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-23 10:53:21 -0800

    ChristmasTree

--- src/ChristmasTree.java
+++ src/ChristmasTree.java
@@ -26,55 +26,67 @@ public class ChristmasTree {
 
     void drawTreeBody() {
         // 8. Change the color of the line the robot draws to forest green    
-
+rob.setPenColor(124,252,0);
+rob.setSpeed(100);
+rob.penDown();
 
         // 1. Make a variable for turnAmount and set it to 175
-
+int turnAmount=175;
 
         // 2. Start the Robot facing to the right
-
+rob.setAngle(90);
 
         // 5. Repeat steps 3 through 11, 11 times
+for(int i=0; i<11; i++) {
+	//code
+
 
 
             // 3. Move the robot the width of the tree
-    
-            // 4. Turn the robot the current turnAmount to the right
-    
+  rob.move((int) treeWidth);
+		  // 4. Turn the robot the current turnAmount to the right
+    rob.turn(turnAmount);
             // 6. Set the treeWidth to the current treeWidth times the scale
-    
+    treeWidth=treeWidth*scale;
             // 7. Move the robot the width of a tree again
-    
+    rob.move((int) treeWidth);
             // 9. Turn the robot the current turn amount to the LEFT
-    
+    rob.turn(-turnAmount);
             // 10. Set the treeWidth to the current treeWidth times the scale again
-    
+    treeWidth=treeWidth*scale;
             // 11. Decrease turnAmount by 1
-
-
+turnAmount=turnAmount-1;
 }
+}
+
     
     void drawTreeTrunk() {
         // 1. Move the robot half the width of the tree
-
+rob.move((int) treeWidth/2);
 
         // 2. Change the robot so that it is pointing straight down
-
+rob.setAngle(180);
 
         // 4. Set the pen width to the tree width divided by 10
-
+rob.setPenWidth((int) (treeWidth/10));
 
         // 5. Change the color of the line the robot draws to brown
-
+rob.setPenColor(122,86,36);
 
         // 3. Move the robot a quarter the tree width
-
-
+rob.move((int) (treeWidth/4));
+rob.hide();
     }
     
     void drawStar() {
         // * Optional: Draw a red star on top of the tree. Hint: 144 degrees makes a star.
-    
+  rob.penDown();
+  rob.setSpeed(500);
+    	rob.setPenColor(255,0,0);
+    	for(int i=0; i<5; i++) {
+    	rob.move(100);
+    	rob.turn(144);
+    }
     }
 
 
